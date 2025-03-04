cb758ae74b75fc35bb3102a9eb26e48afe560714
lucas <accaunoff99@gmail.com>
2025-03-04 22:36:42 +0900

    PROJECT: FEAT: add shopping-list-item com/ make another functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,42 @@ import ShoppingList from "./shopping-list"
 import Information from './information'
 
 function App() {
+  const data = [
+    {
+      id:1,
+      size:14,
+      title:"Buy Bananas",
+      active:false
+    },
+    {
+      id:2,
+      size:11,
+      title:"Buy Kiwi",
+      active:true 
+    },
+    {
+      id:3,
+      size:10,
+      title:'Buy Milk',
+      active:false
+    },
+   
+  ]
   return (
     <>
 <div className="app">
   <div className="wrapper">
     <div className="card"> 
     <Information/>
-    <ShoppingAddForm/>
-      <ShoppingList/>
+    <ShoppingAddForm />
+      <ShoppingList data={data}/> 
       <Filter/>
-      
-     
     </div>
     <img src="/earth.svg" alt="svg" />
   </div>
 </div>
     </>
+    
   )
 }
 
--- src/components/shoping-add-form.jsx
+++ src/components/shoping-add-form.jsx
@@ -1,11 +1,37 @@
-const ShoppingAddForm = () => {
-	return (
-		<div className='form'>
-			<input type="text" className='title' placeholder='Title...' />
-			<input type='number' className='number' placeholder='14' />
-			<button>Add</button>
-		</div>
-	)
+import React from "react"
+class ShoppingAddForm extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={
+			title:'Buy',
+			number:'0'
+		}
+	}
+	onChange = (e)=>{this.setState({[e.target.name]: e.target.value})}
+	render(){
+		const {number,title} = this.state
+		return (
+			<div className='form'>
+				<input
+				 type="text" 
+				 className='title'
+				  placeholder='Title...'
+				  name='title'
+				  onChange={this.onChange}
+				  value={title} />
+
+				<input 
+				type='number'
+				 className='number'
+				  placeholder='14'
+				  name="number" 
+				  onChange={this.onChange}
+				  value={number}/>
+				  
+				<button>Add</button>
+			</div>
+		)
+	}
 }
 
 export default ShoppingAddForm
